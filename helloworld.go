981aeb02dd7b547af7b89e6dc13a3c478eac7e61
77finnge <1041143432@qq.com>
2021-09-30 11:13:24 +0800

    add testgo

---
+++ helloworld.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode) {
+	var head *TreeNode
+	head = root
+	preorder(root, head)
+	root = head
+}
+
+func preorder(root *TreeNode, mainroot *TreeNode) {
+
+	if root == nil {
+		return
+	} else {
+		mainroot.Right = root
+		mainroot.Left = new(TreeNode)
+		mainroot = root
+		preorder(root.Left, mainroot)
+		preorder(root.Right, mainroot)
+	}
+}
+
